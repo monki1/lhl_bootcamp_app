9a5adae0a3c72826a58af71c5d6b7eb23de66910
monki1 <114178697+monki1@users.noreply.github.com>
2023-05-31 10:11:18 -0400

    reference pool.js for initialization + fix bug

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,14 +1,5 @@
-const { Pool } = require('pg');
+const pool = require('./pool');
 
-const pool = new Pool({
-  user: 'mw',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
-});
-
-const cohortName = process.argv[2]; // Get the cohort name from command line argument
-const limit = parseInt(process.argv[3]); // Get the limit from command line argument and parse it as an integer
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -16,7 +7,7 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = '${process.argv[2]}'
 ORDER BY teacher;
 `)
 .then(res => {
